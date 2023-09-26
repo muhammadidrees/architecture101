333a97852eba361bc132743e82de2190c2b66a5d
Muhammad Idrees <midrees097@gmail.com>
2023-09-26 16:25:12 +0500

    clean up

--- overly_engineered_counter/packages/counter_repository/example/counter_repository_example.dart
+++ overly_engineered_counter/packages/counter_repository/example/counter_repository_example.dart
@@ -1,6 +1,17 @@
-// import 'package:counter_repository/counter_repository.dart';
+import 'package:counter_repository/counter_repository.dart';
 
-// void main() {
-//   var awesome = Awesome();
-//   print('awesome: ${awesome.isAwesome}');
-// }
+void main() async {
+  final counterRepository = CounterInMemoryRepository();
+  final counter = counterRepository.counter;
+
+  counter.listen((counter) {
+    print('Counter: $counter');
+  });
+
+  await counterRepository.counter.first;
+
+  await counterRepository.increment();
+  await counterRepository.increment();
+
+  counterRepository.close();
+}
